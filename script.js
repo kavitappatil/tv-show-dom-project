ad409400296db9cf69954fca73f8483cc8114dc0
kavitappatil <patil.poem@gmail.com>
2022-10-14 16:48:06 +0100

    completed Level 300

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
+let rootElem = document.getElementById("episodeGrid");
 let searchInput = document.getElementById("search");
+let selectEpisodeSelector = document.getElementById("episodeSelector");
 let allEpisodes = [];
 
 //You can edit ALL of the code here
 function setup() {
   allEpisodes = getAllEpisodes(); 
   showEpisodes(allEpisodes); //73
+
+  //load episodeSelector dropdown
+  populateEpisodeSelector(allEpisodes);
 
    // show valid search count
   validSearchCount = document.getElementById("validSearchCount");
@@ -16,10 +21,10 @@ function setup() {
 }
 
 function showEpisodes(episodeList) {
-  const rootElem = document.getElementById("episodeGrid");
 
   // Start for loop
   //for (i = 0; i < episodeList.length; i++) {
+    //episodeList[i].name;
     
   episodeList.forEach((elem) => {
     let divEpisode = document.createElement("div");
@@ -60,12 +65,38 @@ function showEpisodes(episodeList) {
   });
 
   //end for loop
+  //---------------------
 
   //rootElem.innerHTML = `Got ${episodeList.length} episode(s)`;
 }
 
+function populateEpisodeSelector(allEpisodes) {
+
+  // Add options to selectEpisodeSelector
+  //<option value="4952"><span>Winter is Coming</span><span> - S01E01</span></option>
+    allEpisodes.forEach((elem) => {
+      //create option element
+    let optionElement = document.createElement("option");
+      optionElement.setAttribute("value", elem.id);
+      //create span element
+    let episodeName = document.createElement("span");
+      //create span element
+    let episodeNumber = document.createElement("span");
+     //assign values to elements
+    episodeName.innerHTML = elem.name;
+    episodeNumber.innerHTML = ` - S${elem.season}E${elem.number}`;
+     
+    optionElement.appendChild(episodeName);
+    optionElement.appendChild(episodeNumber);
+    //append to selectEpisodeSelector
+    selectEpisodeSelector.appendChild(optionElement);
+    
+    
+});
+  
+}
+
 function clearEpisodes() {
-  const rootElem = document.getElementById("episodeGrid");
   rootElem.innerHTML = "";
 }
 
@@ -85,7 +116,7 @@ searchEpisodes = (e, allEpisodes) => {
   });
 
   // clear currently shown on screen
- clearEpisodes();
+  clearEpisodes();
 
   //show found episodes
   console.log(foundEpisodes);
@@ -100,6 +131,46 @@ searchInput.addEventListener("input", (e) => {
   console.log(e.target.value);
   searchEpisodes(e, allEpisodes);
 });
+
+//search episodes using Select ID
+function searchEpisodesUsingId(e, allEpisodes) {
+
+  console.log(e.target.value);
+  let foundEpisodes = [];
+
+  //filter 
+  if(e.target.value != 0)
+  {
+    foundEpisodes = allEpisodes.filter((episode) => {
+    return (
+        episode.id == e.target.value
+      )
+    });
+  }
+  else {
+    foundEpisodes = allEpisodes;
+  }
+
+  // clear currently shown on screen
+  clearEpisodes();
+
+  //show found episodes
+  console.log(foundEpisodes);
+  showEpisodes(foundEpisodes); // 1
+
+  // show valid search count
+  validSearchCount = document.getElementById("validSearchCount");
+  validSearchCount.innerHTML = `Displaying ${foundEpisodes.length}/${allEpisodes.length} episodes`;
+  
+}
+
+selectEpisodeSelector.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  searchEpisodesUsingId(e, allEpisodes);
+});
+
+
+
 
 window.onload = setup;  
 
